42b2475b0734c7718d6b1ac3e561ec2c03a3e2fd
belisarius <belisarius@One.axa.xx>
2016-01-19 14:27:57 +0100

    first commit of the project

---
+++ main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+	"os"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/gorilla/mux"
+	"github.com/influxdb/influxdb/client/v2"
+)
+
+// define global vars
+var (
+	influx_user      string
+	influx_pass      string
+	InfluxClient     client.Client
+	InfluxPointbatch client.BatchPoints
+)
+
+func init() {
+	// Log as JSON instead of the default ASCII formatter.
+	log.SetFormatter(&log.JSONFormatter{})
+
+	// Output to stderr instead of stdout, could also be a file.
+	log.SetOutput(os.Stdout)
+
+	// Only log the warning severity or above.
+	log.SetLevel(log.InfoLevel)
+}
+
+func Handlers() *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/db/graphite/series", HandleInflux).Methods("POST")
+	return r
+}
+
+func main() {
+	// local vars
+	var err error
+
+	addr := flag.String("bind", ":8089", "on which address should the proxy listen")
+	dest := flag.String("dest", "http://localhost:8086", "url to send the Influxdb 0.9 metrics")
+	db := flag.String("db", "graphite", "Influxdb 0.9 database to use")
+	user := flag.String("user", "influx", "Influxdb 0.9 user")
+	pass := flag.String("password", "secret", "Influxdb 0.9 password")
+	verbose := flag.Bool("verbose", false, "set verbose messages ON")
+	debug := flag.Bool("debug", false, "set debug ON")
+	flag.Parse()
+
+	// set the right logging level
+	if *verbose == true {
+		log.SetLevel(log.InfoLevel)
+	} else if *debug == true {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
+	}
+
+	log.WithFields(log.Fields{
+		"bind": addr,
+	}).Info("Influxdb 0.8 0.9 proxy started")
+
+	influx_user = *user
+	influx_pass = *pass
+
+	// connect to the destination influxdb 0.9 server
+	conf := client.HTTPConfig{
+		Addr:     *dest,
+		Username: *user,
+		Password: *pass,
+	}
+
+	InfluxClient, err = client.NewHTTPClient(conf)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"address": *dest,
+			"user":    *user,
+			"error":   err.Error(),
+		}).Fatal("Can't connect to Influxdb 0.9")
+	}
+
+	// Create a new global batch point handler
+	InfluxPointbatch, _ = client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  *db,
+		Precision: "us",
+	})
+
+	http.ListenAndServe(*addr, Handlers())
+}
---
+++ routes.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"math"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/influxdb/influxdb/client/v2"
+)
+
+// this structure defines the JSON message format of Influxdb 0.8
+type influxStruct struct {
+	Name    string      `json:"name"`
+	Columns []string    `json:"columns"`
+	Points  [][]float64 `json:"points"`
+}
+
+type influxMultiStruct []influxStruct
+
+// this is the main function to process an Influxdb 0.8 request
+func HandleInflux(w http.ResponseWriter, r *http.Request) {
+
+	// instanciate an Influxdb data structure
+	t := influxMultiStruct{}
+
+	jsonDataFromHttp, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Warn("Body parsing Error, dropping request")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	err = json.Unmarshal(jsonDataFromHttp, &t)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Warn("Body JSON parsing Error, dropping request")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	// check the params
+	err = r.ParseForm()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Warn("URL parsing Error, dropping request")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"RemoteAddr":      r.RemoteAddr,
+		"X-Forwarded-For": r.Header["X-Forwarded-For"],
+		"RequestURI":      r.RequestURI,
+	}).Info("Ready to parse request")
+
+	// we process only if login/pass is good
+	// for the moment the login/pass to remote influx is fixed, but we should
+	// send the data with the incoming login/pass
+
+	if r.Form["u"][0] == influx_user && r.Form["p"][0] == influx_pass {
+		// parse the metrics and add to the queue
+		io.WriteString(w, "request accepted for user "+influx_user+"\n")
+	} else {
+		io.WriteString(w, "Username/Password missmatch\n")
+		log.WithFields(log.Fields{
+			"RemoteAddr":      r.RemoteAddr,
+			"X-Forwarded-For": r.Header["X-Forwarded-For"],
+			"RequestURI":      r.RequestURI,
+		}).Warn("Username/Password missmatch")
+		return
+	}
+
+	// parse the json
+	var pointTime time.Time
+	for index, element := range t {
+		log.WithFields(log.Fields{
+			"id":      index,
+			"name":    element.Name,
+			"columns": element.Columns,
+			"points":  element.Points,
+		}).Debug("Adding a new point")
+
+		// split the serie name in master name and tags
+		serieSplit := strings.Split(element.Name, ".")
+
+		// generate tags from the doted name
+		tags := make(map[string]string)
+		tags["name"] = element.Name
+		for index, tag := range serieSplit {
+			tags["tag"+strconv.Itoa(index)] = tag
+		}
+
+		// add each points of the same serie
+		for _, point := range element.Points {
+			// splitting fields
+			fields := make(map[string]interface{})
+			for index, field := range element.Columns {
+				fields[field] = point[index]
+			}
+
+			if field, ok := fields["time"]; ok {
+				// parse the time
+				switch {
+				case r.Form["time_precision"][0] == "s":
+					pointTime = time.Unix(int64(field.(float64)), 0)
+
+				case r.Form["time_precision"][0] == "ms":
+					timeSec := math.Abs(field.(float64) / 1000)
+					timeNs := math.Mod(field.(float64), 1000)
+					pointTime = time.Unix(int64(timeSec), int64(timeNs))
+
+				case r.Form["time_precision"][0] == "us":
+					timeSec := math.Abs(field.(float64) / 1000000)
+					timeNs := math.Mod(field.(float64), 1000000)
+					pointTime = time.Unix(int64(timeSec), int64(timeNs))
+
+				default:
+					pointTime = time.Now()
+					log.WithFields(log.Fields{
+						"id": index,
+					}).Info("no time provided, using local time ", pointTime)
+				}
+
+				// finaly remove the time entry as it is a special value
+				delete(fields, "time")
+
+			} else {
+				pointTime = time.Now()
+				log.WithFields(log.Fields{
+					"id": index,
+				}).Info("no time provided, using local time ", pointTime)
+			}
+
+			// create a new point
+			pt, _ := client.NewPoint(serieSplit[0], tags, fields, pointTime)
+			InfluxPointbatch.AddPoint(pt)
+			log.WithFields(log.Fields{
+				"metric": serieSplit[0],
+				"time":   pointTime,
+			}).Info("new Metric added")
+		}
+	}
+	err = InfluxClient.Write(InfluxPointbatch)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Warn("error adding Metric")
+		return
+		//fmt.Println("Error: ", err.Error())
+	}
+}
